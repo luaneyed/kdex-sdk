ce98ad48e485f5cd96016ac539a054e11ede6d30
Sangguk Lee <sangguk258@gmail.com>
2021-10-14 18:38:13 +0900

    Fix test code

--- test/constants.test.ts
+++ test/constants.test.ts
@@ -14,7 +14,7 @@ describe('constants', () => {
     // })
     //TODO: Replace with real PancakePair.json
     it('matches computed bytecode hash', () => {
-      expect(INIT_CODE_HASH).toEqual('0xd0d4c4cd0848c93cb4fd1f498d7013ee6bfb25783ea21593d5834f5d250ece66')
+      expect(INIT_CODE_HASH).toEqual('0xe26f1edeb3c91a91d1eabfa8c3a718cd73b3b1abe7d4228919cb302be3985e52')
     })
   })
 })
